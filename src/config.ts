dde6c4809281549892185f58078eac6af684b941
Illia Khovanskyi <saskiuhia@gmail.com>
2026-04-29 22:28:52 +0200

    Env var fix

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,9 @@
-export const API_BASE_URL = window._env_?.API_BASE_URL || "http://localhost:8080";
+function tryToGetEnv(key: keyof Window['_env_'], defaultValue: string): string {
+    const value = window._env_?.[key];
+    if (!value || value === `\${${key}}`)
+        return defaultValue;
+
+    return value;
+}
+
+export const API_BASE_URL = tryToGetEnv('API_BASE_URL', 'http://localhost:8085');
